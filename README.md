66a6041addb5bf7a2947baea2083edd2199bb795
Logan <monday.hopscotch@gmail.com>
2020-05-31 00:24:59 -0600

    Hide async within this library

---
+++ README.md
@@ -0,0 +1,11 @@
+This is currently not a published library
+
+To use:
+
+1. Install the library via haxelib: 
+
+`haxelib git bitdecayanalytics https://github.com/bitDecayGames/Analytics.git`
+
+2. Add the dependency in your project (`project.xml` for HaxeFlixel):
+
+`<haxelib name="bitdecayanalytics" />`
--- com/bitdecaygames/analytics/Analytics.hx
+++ com/bitdecaygames/analytics/Analytics.hx
@@ -1,15 +1,19 @@
 package com.bitdecaygames.analytics;
 
+import hx.concurrent.executor.Executor;
 import net.lion123dev.GameAnalytics;
 import net.lion123dev.events.Events.GAPlatform;
 
 class Analytics {
 	private static var ga:GameAnalytics;
+	private static var exec:Executor;
+	private static var analyticsStarter:TaskFuture<Bool>;
 
 	public static function Instance():GameAnalytics {
 		if (ga == null) {
 			trace("Analytics not init'ed. Creating Sandbox instance");
 			Init("5c6bcb5402204249437fb5a7a80a4959", "16813a12f718bc5c620f56944e1abc3ea13ccbac", true);
+			analyticsStarter.waitAndGet(-1);
 		}
 
 		return ga;
@@ -32,19 +36,32 @@ class Analytics {
 			return;
 		}
 
-		trace("init'ing analytics");
 		ga = new GameAnalytics(gameKey, secret, sandbox);
+		setExitHandler(function() {
+			trace("closing down analytics");
+			Close();
+		});
 
+		exec = Executor.create();
+		analyticsStarter = exec.submit(startAnalytics, Schedule.ONCE(0));
+	}
+
+	private static function startAnalytics():Bool {
+		trace("init'ing analytics");
 		ga.Init(onSuccess, onFail, GAPlatform.WINDOWS, GAPlatform.WINDOWS + " 10", "unknown", "unknown");
 
 		ga.OnSubmitFail = onFail;
 		ga.OnSubmitSuccess = onSuccess;
 		ga.StartPosting();
+		return true;
+	}
 
-		setExitHandler(function() {
-			trace("closing down analytics");
-			Close();
-		});
+	public static function Ready():Bool {
+		// make sure we have an instance to BE ready
+		Instance();
+
+		// report if our starter has finished
+		return analyticsStarter != null && analyticsStarter.isStopped;
 	}
 
 	// Now we need to initialize it with success and fail callbacks, platform, os version, device and manufacturer. Some of these parameters may be removed later and auto generated instead.
